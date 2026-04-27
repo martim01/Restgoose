a80874c5ba90a4635f33d00b5efe282852532704
Matthew Martin <matthew.martin@bbc.co.uk>
2026-04-27 08:59:13 +0100

    imp: process timeout to 5000ms

--- include/httpclient.h
+++ include/httpclient.h
@@ -75,7 +75,7 @@ namespace pml::restgoose
             *   @param[in] processTimeout the amount of time to wait for the whole connect, send, receive procedure to take place. If set to 0 then the timeout is ignored
             *   @return clientResponse a clientResponse object containing the HTTP repsonse code and any data sent from the server
             **/
-            const clientResponse& Run(const std::chrono::milliseconds& connectionTimeout = std::chrono::milliseconds(5000), const std::chrono::milliseconds& processTimeout = std::chrono::milliseconds(0)) const;
+            const clientResponse& Run(const std::chrono::milliseconds& connectionTimeout = std::chrono::milliseconds(5000), const std::chrono::milliseconds& processTimeout = std::chrono::milliseconds(5000)) const;
 
             /** @brief The function that attempts to connect to the server, send any data and retrieve any response. This is an asynchronous function
             *   @param[in] pCallback a function to call when the procedure is complete, its arguments are a clientResponse object containing the repsonse from the server, an unsigned int containing the value passed in nRunId and a string containing sUserData
@@ -86,7 +86,7 @@ namespace pml::restgoose
             *   @param[in] delay the amount of time to wait before running the function. This wait time may be longer depending on thread allocation.
             *   @return clientResponse a clientResponse object containing the HTTP repsonse code and any data sent from the server
             **/
-            void Run(const std::function<void(const clientResponse&, unsigned int, const std::string& )>& pCallback, unsigned int nRunId, const std::string& sUserData, const std::chrono::milliseconds& connectionTimeout = std::chrono::milliseconds(5000), const std::chrono::milliseconds& processTimeout = std::chrono::milliseconds(0), const std::chrono::milliseconds& delay = std::chrono::milliseconds(0)) const;
+            void Run(const std::function<void(const clientResponse&, unsigned int, const std::string& )>& pCallback, unsigned int nRunId, const std::string& sUserData, const std::chrono::milliseconds& connectionTimeout = std::chrono::milliseconds(5000), const std::chrono::milliseconds& processTimeout = std::chrono::milliseconds(5000), const std::chrono::milliseconds& delay = std::chrono::milliseconds(0)) const;
 
 
             /** @brief The function that attempts to connect to the server, send any data and retrieve any response. This is an asynchronous function
@@ -97,7 +97,7 @@ namespace pml::restgoose
             *   @param[in] delay the amount of time to wait before running the function. This wait time may be longer depending on thread allocation.
             *   @return clientResponse a clientResponse object containing the HTTP repsonse code and any data sent from the server
             **/
-            void Run(const std::function<void(const clientResponse&, unsigned int)>& pCallback, unsigned int nRunId, const std::chrono::milliseconds& connectionTimeout = std::chrono::milliseconds(5000), const std::chrono::milliseconds& processTimeout = std::chrono::milliseconds(0), const std::chrono::milliseconds& delay = std::chrono::milliseconds(0)) const;
+            void Run(const std::function<void(const clientResponse&, unsigned int)>& pCallback, unsigned int nRunId, const std::chrono::milliseconds& connectionTimeout = std::chrono::milliseconds(5000), const std::chrono::milliseconds& processTimeout = std::chrono::milliseconds(5000), const std::chrono::milliseconds& delay = std::chrono::milliseconds(0)) const;
 
             /** @brief Sets a callback function that is called every time a "chunk" of data is sent to the server. This is useful for showing the progress of large uploads
             *   @param[in] pCallback the callback function. It is passed two values: the first is the number of bytes uploaded and the second the total number of bytes that will be uploaded
